71a24cbd460e5a21734df4642eaa50908ec8d95b
chuan <yichiuan326@gmail.com>
2024-05-31 16:51:26 +0800

    Updata Card

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,10 @@
+/*
+  @author 廖倚詮 <B11053020@gemail.yuntech.edu.tw>
+*/
+export default function CustomCard({ item }) {
+    return (
+        <div className="border">
+            <div>{item.ScenicSpotName}</div>
+        </div>
+    )
+}
